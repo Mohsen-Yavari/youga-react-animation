1616f1d39497966c489ab93f92abdb295c63cb13
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-21 15:20:14 +0330

    crate data file informations

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react';
+
+//import menu btn icon
+import { BiMenu } from 'react-icons/bi';
 
 const NavMobile = () => {
   return (
-    <div>NavMobile</div>
+    <nav>
+        {/* menu btn */}
+        <div className="cursor-pointer text-4xl text-heading
+        ml-[10px] lg:hidden ">
+            <BiMenu />
+        </div>
+        {/* menu items */}
+    </nav>
   )
 }
 
