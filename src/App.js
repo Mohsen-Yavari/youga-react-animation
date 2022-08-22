e9456ee6cfa6ed33cddc65e8df1677e0dc021743
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-22 11:01:59 +0330

    frature component finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Cards from './components/Cards';
 import Facts from './components/Facts';
+import Features from './components/Features';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <Cards />
       <Facts />
+      <Features />
       <div className="h-[2000px]"></div>
     </div>
   );
