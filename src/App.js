6ae1b3b368a18ab49596878aefbaa38353d9094b
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-22 09:16:17 +0330

    install react-countent and created facts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import Cards from './components/Cards';
+import Facts from './components/Facts';
 
 function App() {
   return (
     <div className="overflow-hidden">
       <Hero />
       <Cards />
+      <Facts />
       <div className="h-[2000px]"></div>
     </div>
   );
